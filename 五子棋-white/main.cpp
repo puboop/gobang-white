bc8957df9b12f5f190ed6b04ce69201e89ec45f6
white-test <qwhite@163.com>
2023-09-09 23:28:19 +0800

    单机模式修改完毕

--- 五子棋-white/main.cpp
+++ 五子棋-white/main.cpp
@@ -1,18 +1,70 @@
 #include "piece.h"
 
-int boardPoint[call_count][call_count]; // 棋盘坐标数组
 int piece_color = 0; // 0 为白色，1为黑色
+extern bool is_exec_piece = true; // 执行下棋，当游戏结束后结束落棋子
+Piece* piece_map[call_count][call_count]; // 初始化出地图一样的结构体数组
 
 int main() {
-	printf("输出>%d\n", 35 / 30);
-	//initBoard(boardPoint); // 初始化棋盘
-	//ExMessage mouse;// 声明鼠标消息结构体
-	//drawPiece(piece_color, 450, 450);
-	//while (1) {
-	//	getmessage(&mouse, EX_MOUSE); // 获取鼠标消息
-	//	if (mouse.message == WM_LBUTTONDOWN) {// 是否是左击消息
-	//		piece_color = leftClick(piece_color, mouse, boardPoint);
-	//	}
-	//}
+	initgraph(boardX_extend, boardY_extend); // 定义窗口大小
+	// 创建按钮
+	Button* replay = createButton(30, boardX +30, 120, 40, "重玩", RGB(248, 227, 200), board_color);
+	Button* playback = createButton(30+120+30, boardX + 30, 120, 40, "回放", RGB(248, 227, 200), board_color);
+	Button* exit_play = createButton(30+120+ 120 +30+30, boardX + 30, 120, 40, "退出游戏", RGB(248, 227, 200), board_color);
+
+	initBoard(replay, playback, exit_play); // 初始化棋盘 得到一个头节点
+	Piece* piece;
+	piece = initPieceLink();// 初始化节点链表
+	Piece* playback_piece;
+	playback_piece = initPlayLinkNode();
+	initArray(piece_map); // 初始化数组
+
+	ExMessage mouse;// 声明鼠标消息结构体
+	while (1) {
+		BeginBatchDraw();
+		getmessage(&mouse, EX_MOUSE); // 获取鼠标消息
+		if (is_exec_piece && mouse.message == WM_LBUTTONDOWN) {// 是否是左击消息
+			piece_color = leftClick(piece_color, mouse, piece, playback_piece, piece_map);
+		}
+		if (isClickButton(replay, mouse)) {// 点击按钮消息
+			deleteLink(piece);// 销毁链表
+			deleteLink(playback_piece);// 销毁链表
+			deleteArray(piece_map); // 释放结构体数组
+
+			initBoard(replay, playback, exit_play); // 初始化棋盘 得到一个头节点
+			piece = initPieceLink();// 初始化节点链表
+			playback_piece = initPlayLinkNode();
+			initArray(piece_map); // 初始化数组
+			piece_color = 0;
+		}
+		if (isClickButton(playback, mouse)) {// 点击按钮消息
+			initBoard(replay, playback, exit_play); // 初始化棋盘 得到一个头节点
+			back_play_piece(playback_piece); // 回放处理
+		}
+		if (isClickButton(exit_play, mouse)) {// 点击按钮消息
+			deleteLink(piece);// 销毁链表
+			deleteLink(playback_piece);// 销毁链表
+			deleteArray(piece_map); // 释放结构体数组
+			break;
+		}
+		FlushBatchDraw();  // 刷新缓存
+	}
+	EndBatchDraw();
+	closegraph();
+	//////// 测试用例 ///////////
+	//initArray(piece_map);
+	//for (int i = 0; i < call_count; i++)
+	//	for (int j = 0; j < call_count; j++)
+	//		printf("x>%d, y>%d, seat>%d\n", piece_map[i][j]);
+	//initgraph(400, 400);
+	//setbkcolor(RGB(248, 227, 200));
+	//cleardevice();
+	//settextcolor(RED); // 设置文字颜色
+	//setbkmode(TRANSPARENT); // 去掉文字背景
+	//settextstyle(35, 0, "楷体"); // 设置文字样式
+	//char text[] = "白棋赢！";
+	//outtextxy(10, 20, text);
+	//while (1);
+	//system("pause");
+	///////////////////////////////
 	return 0;
 }
--- 五子棋-white/piece.h
+++ 五子棋-white/piece.h
@@ -1,15 +1,55 @@
 #pragma once
 #include <graphics.h>
 #include <stdio.h>
+#include <math.h>
+#include <windows.h>
+#include <stdbool.h>
+#include <assert.h>
+#include <windows.h>
+
 #define call_count 15 // 表示格子数 单位像素
 #define call_size 30  // 表示每个格子的长 单位像素
 #define board_extend call_size // 棋盘向外扩展的区域大小
 #define boardX (call_count * call_size) // 棋盘区域大小
 #define boardY (call_count * call_size) // 棋盘区域大小
 #define boardX_extend (call_count * call_size + board_extend) // 棋盘扩展后的大小
-#define boardY_extend (call_count * call_size + board_extend) // 棋盘扩展后的大小
+#define boardY_extend (call_count * call_size + board_extend*3) // 棋盘扩展后的大小
+#define board_color RGB(248, 227, 200) // 棋盘颜色
+typedef struct Piece { // 棋子结构体
+	int x; // 当前棋子的x坐标
+	int y; // 当前棋子的y坐标
+	int seat; // 当前放的棋子 -1 未放棋子，0 白棋子 1 黑棋子
+	struct Piece* next; // 指向下一个节点
+};
 
+typedef struct Button { // 按钮结构体
+	int y;
+	int x;
+	int w;
+	int h;
+	COLORREF curColor; // 按钮当前颜色
+	COLORREF inColor; // 鼠标在按钮里面的颜色
+	COLORREF outColor; // 鼠标不在按钮中的颜色
+	char* text;
+};
 
-void initBoard(int boardPoint[call_count][call_count]); // 初始化棋盘
-int drawPiece(int pc, int x, int y); // 画棋子
-int leftClick(int pc, ExMessage mouse, int boardPoint[call_count][call_count]); // 鼠标左击
+void initBoard(Button* replay, Button* playback, Button* exit_play); // 初始化棋盘
+Piece* initPieceLink(); // 初始化坐标节点链表
+Piece* initPlayLinkNode();// 初始化回放节点链表
+void initArray(Piece* piece_map[call_count][call_count]); // 初始化数组
+int drawPiece(int piece_color, int x, int y); // 画棋子
+int leftClick(int piece_color, ExMessage mouse, Piece* piece, Piece* playback_piece, Piece *piece_map[call_count][call_count]); // 左点击布棋
+bool isWin(Piece* piece_map[call_count][call_count]); // 判断输赢
+void textPrintf(char text[50]); // 向屏幕中输入文本
+void ifIsWho(int seat); // 判断谁赢
+Piece *check_back_node(); // 向当前的回放链表中添加节点
+void back_play_piece(Piece* playback_piece);// 进行节点回放
+Button* createButton(int x, int y,
+	int w, int h,
+	const char* str,
+	COLORREF inColor,
+	COLORREF outColor); // 创建按钮
+void drawButton(Button* pB); // 绘制按钮
+bool isClickButton(Button* pB, ExMessage m); // 鼠标点击按钮
+void deleteLink(Piece* link);// 销毁链表
+void deleteArray(Piece* piece_map[call_count][call_count]); // 释放数组
